1c09fa0412bfba24abe6a4701deda283b6723df8
David Bowers <dbowersinc@hotmail.com>
2022-09-04 16:17:49 -0700

    added Blog Page

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/blog">Blog</RouterLink>
       </nav>
     </div>
   </header>
